cfc5feb2c9e2cc9bfc6b65f531b4820d2ee4fc5b
Julius Williams <doctorj89@gmail.com>
2017-03-24 13:28:23 -0400

    react v1

---
+++ portfolio/src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { browserHistory } from 'react-router'
+
+class Sidebar extends React.Component {
+    render() {
+        return <div className="panel panel-default">
+              <div className="panel-heading">Skills</div>
+
+              <ul className="list-group listItems">
+                <li className="list-group-item">Android</li>
+                <li className="list-group-item">Bootstrap</li>
+                <li className="list-group-item">CSS</li>
+                <li className="list-group-item">Git</li>
+                <li className="list-group-item">HTML</li>
+                <li className="list-group-item">Java</li>
+                <li className="list-group-item">JavaScript</li>
+                <li className="list-group-item">Material Design</li>
+                <li className="list-group-item">Project Management</li>
+                <li className="list-group-item">UI/UX Design</li>
+                <li className="list-group-item">XML</li>
+              </ul>
+              <div className="panel-heading">Tools</div>
+              <ul className="list-group listItems">
+                <li className="list-group-item">Android Studio</li>
+                <li className="list-group-item">Balsamiq</li>
+                <li className="list-group-item">IntelliJ</li>
+                <li className="list-group-item">Git</li>
+                <li className="list-group-item">Mac</li>
+                <li className="list-group-item">Visual Studio</li>
+                <li className="list-group-item">Windows</li>
+              </ul>
+            </div>
+    }
+}
+
+export default Sidebar
---
+++ portfolio/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Work from './Work';
+// import WorkSample from
+
+import Routes from './Routes';
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
